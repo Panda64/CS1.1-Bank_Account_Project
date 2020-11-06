f4435af866727cc79e4fde7672c2a3821d037978
Panda64 <JaylenSchelb@yahoo.com>
2020-11-05 23:46:37 -0800

    Added all but one method (tested and working)

--- BankAccount.py
+++ BankAccount.py
@@ -15,14 +15,18 @@ class BankAccount:
     balance = 0
 
     def deposit(self, amount):
+        amount = float(amount)
+
         self.balance += amount
 
         print(f"Amount Deposited: ${amount}")
 
     def withdrawl(self, amount):
+        amount = float(amount)
+
         if amount > self.balance:
             print("Insufficient funds. You will be charged a $10 overdraft fee if you withdraw this amount. \
-                   Do you wish to continue? (y/n) ")
+Do you wish to continue? (y/n) ")
             
             retry = True
 
@@ -31,7 +35,7 @@ class BankAccount:
 
                 if user_input == 'y' or user_input == 'Y':
                     self.balance -= (amount + 10)
-                    print(f"Withdraw confirmed, you will be charged a $10 overdraft fee. Amount Withdrawn: ${amount}")
+                    print(f"Withdraw confirmed, you have been charged a $10 overdraft fee. Amount Withdrawn: ${amount}")
                     retry = False
                 elif user_input == 'n' or user_input == 'N':
                     "Withdraw canceled."
@@ -40,9 +44,14 @@ class BankAccount:
                     print("Please type either 'y' or 'n'. Try again.")
         else:
             self.balance -= amount
+            print(f"Amount Withdrawn: ${amount}")
 
     def get_balance(self):
-        print(f"Your current account balance is ${self.balance}")
+        if  self.balance >= 0:
+            print(f"Your current account balance is ${self.balance}")
+        else:
+            self.balance = self.balance * -1
+            print(f"Your current account balance is -${self.balance}")
 
         return self.balance
 
@@ -50,4 +59,63 @@ class BankAccount:
         interest = self.balance *  0.00083
         self.balance += interest
 
-    
+
+restart = True
+
+while restart:
+    print("Welcome to your fictional bank account! To get started, enter your full name below:")
+
+    users_name = str(input())
+
+    account = BankAccount(users_name)
+
+    def deposit():
+        print("How much money would you like to deposit?")
+
+        deposit_amount = input('$')
+
+        account.deposit(deposit_amount)
+
+    def withdrawl():
+        print("How much money would you like to withdrawl?")
+
+        withdrawl_amount = input('$')
+
+        account.withdrawl(withdrawl_amount)
+
+    def get_balance():
+        account.get_balance()
+
+    def add_monthly_interest():
+        account.add_monthly_interest()
+
+    menu = True
+
+    while menu:
+        print("\n\nChoose what you would like to do by typing in the number corresponding to your desired option. \n \
+            1. Deposit \n \
+            2. Withdrawl \n \
+            3. Get your balance \n \
+            4. View your current account receipt \n \
+            5. Add monthly interest (ADMIN ONLY) \n \
+            6. New account \n \
+            7. Quit")
+
+        user_choice = input() 
+
+        if user_choice == "1":
+            deposit()
+        elif user_choice == "2":
+            withdrawl()
+        elif user_choice == "3":
+            get_balance()
+        elif user_choice == "5":
+            add_monthly_interest()
+        elif user_choice == "6":
+            menu = False
+        elif user_choice == "7":
+            menu = False
+            restart = False
+        else:
+            print("Please enter a valid option. Try again.")
+
